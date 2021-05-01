8e02ca544e0cfb2aa24b46b34362651872be2ddb
numanjvd82 <nomanmani62@gmail.com>
2021-05-01 15:22:03 +0500

    created mock data for my products

---
+++ src/mock/data.js
@@ -0,0 +1,32 @@
+import { nanoid } from 'nanoid';
+
+[
+  {
+    id: nanoid(),
+    name: 'Yello Jacket Large',
+    price: 50,
+    image: '../images/Jacket-1.jpg',
+    amount: 2,
+  },
+  {
+    id: nanoid(),
+    name: 'Brown Jacket Small',
+    price: 67,
+    image: '../images/Jacket-2.jpg',
+    amount: 1,
+  },
+  {
+    id: nanoid(),
+    name: 'Blue LA Cap',
+    price: 35,
+    image: '../images/Cap.jpg',
+    amount: 4,
+  },
+  {
+    id: nanoid(),
+    name: 'iPhone X with earpods',
+    price: 800,
+    image: '../images/iPhone.jpg',
+    amount: 3,
+  },
+];
